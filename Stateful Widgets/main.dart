dc9f1610a526a1c343be8dc9fa13bdc724e2ade5
Woojin Song <woojin1900@gmail.com>
2024-12-12 16:20:18 +0900

    StatefulWidget counter

---
+++ Stateful Widgets/main.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatefulWidget {
+  const App({super.key});
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  int counter = 0;
+  List<int> numbers = [];
+
+  void onClicked() {
+    setState(() {
+      counter += 1;
+      numbers.add(counter);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: const Color(0xFFF4EDDB),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text(
+                "Click Count",
+                style: TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              Text(
+                "$counter",
+                style: const TextStyle(
+                  fontSize: 30,
+                ),
+              ),
+              for (var n in numbers) Text("$n"),
+              IconButton(
+                onPressed: onClicked,
+                icon: const Icon(Icons.add_box_rounded),
+                iconSize: 60,
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
