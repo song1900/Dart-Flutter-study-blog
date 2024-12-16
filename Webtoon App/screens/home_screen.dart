74cb579a136718c83790162d0ceab939735f21c3
woojin <woojin1900@gmail.com>
2024-12-16 14:21:18 +0900

    ListView, load image

--- Webtoon App/screens/home_screen.dart
+++ Webtoon App/screens/home_screen.dart
@@ -27,10 +27,73 @@ class HomeScreen extends StatelessWidget {
         future: webtoons,
         builder: (context, snapshot) {
           if (snapshot.hasData) {
-            return Text("There is data!");
+            return Column(
+              children: [
+                SizedBox(
+                  height: 50,
+                ),
+                Expanded(
+                  child: makeList(snapshot),
+                )
+              ],
+            );
           }
-          return Text("Loading...");
+          return Center(
+            child: CircularProgressIndicator(
+              color: Colors.deepPurpleAccent,
+            ),
+          );
         },
+      ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal,
+      itemCount: snapshot.data?.length ?? 0,
+      padding: EdgeInsets.symmetric(
+        vertical: 10,
+        horizontal: 20,
+      ),
+      itemBuilder: (context, index) {
+        var webtoon = (snapshot.data ?? [])[index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              decoration: BoxDecoration(
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 15,
+                    offset: Offset(10, 10),
+                    color: Colors.black.withValues(alpha: 0.5),
+                  )
+                ],
+                borderRadius: BorderRadius.circular(15),
+              ),
+              child: Image.network(
+                webtoon.thumb,
+                headers: {
+                  'Referer': "https://comic.naver.com/",
+                },
+              ),
+            ),
+            SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: TextStyle(
+                fontSize: 22,
+              ),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) => SizedBox(
+        width: 40,
       ),
     );
   }
