1e0b028480fbfab1f59718e86ce6a3a6ae3f5f6f
woojin <woojin1900@gmail.com>
2024-12-16 11:04:51 +0900

    FutureBuilder

--- Webtoon App/screens/home_screen.dart
+++ Webtoon App/screens/home_screen.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:toonflix/models/webtoon_model.dart';
+import 'package:toonflix/services/api_service.dart';
 
 class HomeScreen extends StatelessWidget {
-  const HomeScreen({super.key});
+  HomeScreen({super.key});
+
+  final Future<List<WebtoonModel>> webtoons = ApiService.getTodaysToons();
 
   @override
   Widget build(BuildContext context) {
@@ -18,6 +22,15 @@ class HomeScreen extends StatelessWidget {
             fontWeight: FontWeight.w500,
           ),
         ),
+      ),
+      body: FutureBuilder(
+        future: webtoons,
+        builder: (context, snapshot) {
+          if (snapshot.hasData) {
+            return Text("There is data!");
+          }
+          return Text("Loading...");
+        },
       ),
     );
   }
